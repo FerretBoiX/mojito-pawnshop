e98f8f5f95bcd6aef3320ad25c64aabf6b51c16c
Mojito-Fivem <66041893+Mojito-Fivem@users.noreply.github.com>
2020-08-20 23:34:20 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,14 @@
+Config = {}
+
+Config.PawnShopLocation = vector3(-429.33,-1728.33,19.78)
+
+Config.PawnShopPed = { x = -429.33, y = -1728.33, z = 18.78, h = 75.12 }
+
+Config.PawnShopPedName = "a_m_y_business_01"
+
+Config.PawnShopPrices = {
+    bread = 10,
+    water = 5
+}
+
+Config.GiveDirty = true
